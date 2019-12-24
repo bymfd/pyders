33cbed6a2db766d8e000abb286d8f51bd4150a2d
MFD <frukolosko@gmail.com>
2019-12-24 17:26:24 +0300

    ilk ders
    ilk derste yazılan herşey

---
+++ ders1.py
@@ -0,0 +1,42 @@
+"""
+print("Hello World")
+print('Hello World')
+print("Ankara'da hava nasıl?")
+print("Ankara'da hava nasıl?", "Soğuk")
+print("https","t.me/pcboyutu", sep="//")
+print("T",'C',sep=".")
+print("T",'C',sep=" ")
+print("Naber ","Nasılsın? ","İyimisin",sep="la")
+print("Pardus ve Ubuntu birer GNU/Linux dağıtımıdır.")
+print("birinci","ikinci","üçüncü","dördüncü",sep="\n")
+print("Naber ","Nasılsın? ","İyimisin",sep="la",end=".")
+"""
+
+
+
+
+
+print(1)
+print("a",1,"bdsjsfg",10.1)
+a = 10.1
+b = 12
+c = a
+d = "Mehmet "
+print(d)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a,b,c,sep="+")
+toplam = a+b+c
+print("toplam",toplam)
+print(a+b+c*32)
+print(a*32)
+print(d*2)
+
+# "ahmet" = b
+#
+# print(a,b,c)
+# print(type(a))
+# print(type(b))
+# print(type(c))
